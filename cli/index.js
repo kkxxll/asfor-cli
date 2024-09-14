a85b406a3bf355272c2cbb5ec08d67c9c64977f8
fflinkexin <fflinkexin@chinatelecom.cn>
2024-09-14 15:18:50 +0800

    commander

--- cli/index.js
+++ cli/index.js
@@ -1,22 +1,64 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs");
-const { hideBin } = require("yargs/helpers");
-const lib = require("asfor-lib");
+const commander = require("commander");
 
-console.log("test", lib());
+const pkg = require("../package.json");
+const { options } = require("yargs");
 
-yargs(hideBin(process.argv))
-  .demandCommand(1) // 至少一个参数
-  .command('init [name]', 'create a new project', (yargs) => {
-    yargs.options('name', {
-      type: 'string',
-      describe: 'project name'
-    })
-  }, (argv) => {
-    console.log(argv)
+// 获取 commander 实例
+const { program } = commander;
+
+// 创建一个实例
+// const program = new commander.Command();
+
+// 注册命令
+const clone = program.command("clone <source> [destination]");
+clone
+  .description("clone a repository into a folder")
+  .action((source, destination) => {
+    console.log("123");
+    console.log(source, destination);
+  });
+
+// 注册子命令
+const service = program.command("service");
+service
+  .command("start [port]")
+  .description("start service on port")
+  .action((port) => {
+    console.log("start", port);
+  });
+
+service
+  .command("stop [port]")
+  .description("stop service on port")
+  .action((port) => {
+    console.log("stop", port);
+  });
+
+// 命中所有命令
+program
+  .arguments("<cmd> [options")
+  .description("执行命令", {
+    cmd: "command to run",
+    options: "options for command",
   })
-  .recommendCommands() // 提示命令 如 ini -> init
-  .help("h")
-  .alias("h", "help")
-  .parse()
+  .action((cmd) => {
+    console.log("command", cmd);
+  });
+
+// 自动拼接命令
+program.command("install [name]", "install a package");
+
+// 指定命令执行
+program.command("test-install [name]", "install a package", {
+  executableFile: "test-cli",
+  isDefault: true, // 若命中多个命令，默认执行这个
+  hide: true, // 隐藏，--help 时不会显示
+});
+
+program
+  .name(Object.keys(pkg.bin)[0])
+  .usage("<command> [options]")
+  .version(pkg.version)
+  .parse(process.agrv);
