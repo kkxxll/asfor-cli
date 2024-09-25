35223693c0999c5a203a3fdca0adc35abfe40132
fflinkexin <fflinkexin@chinatelecom.cn>
2024-09-25 10:20:24 +0800

    feat: debug模式

--- packages/cli/lib/createCLI.js
+++ packages/cli/lib/createCLI.js
@@ -1,11 +1,10 @@
-import { program } from "commander";
 import path from "node:path";
+import { program } from "commander";
 import { dirname } from "dirname-filename-esm";
 import fse from "fs-extra";
 import semver from "semver";
 import chalk from "chalk";
 import { log } from "@asfor-cli/utils";
-
 
 const __dirName = dirname(import.meta);
 const pkgPath = path.resolve(__dirName, "../package.json");
@@ -16,7 +15,7 @@ function checkNodeVersion() {
   log.verbose("node version", process.version);
   if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
     throw new Error(
-      chalk.red(`node版本过低,需要安装${LOWEST_NODE_VERSION}以上版本}`)
+      chalk.red(`node版本过低,需要安装${LOWEST_NODE_VERSION}以上版本`)
     );
   }
 }
@@ -33,6 +32,17 @@ export default function createCLI() {
     .version(pkg.version)
     .option("-d, --debug", "是否开启调试模式", false)
     .hook("preAction", preAction);
+
+  program.on("option:debug", function () {
+    if (program.opts().debug) {
+      // debug模式启动
+      log.verbose("debug", "launch debug mode...");
+    }
+  });
+
+  program.on("command:*", function (obj) {
+    log.error("未知的命令", obj[0]);
+  });
 
   return program;
 }
--- packages/cli/lib/index.js
+++ packages/cli/lib/index.js
@@ -5,4 +5,5 @@ import "./exception.js";
 export default function (args) {
   const program = createCLI();
   createInitCommand(program);
+  program.parse(process.argv)
 }
