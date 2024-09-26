a95bfe876e21fb64cb8c1531efd71c5ec8bd44a9
fflinkexin <fflinkexin@chinatelecom.cn>
2024-09-26 19:55:16 +0800

    feat:copy

--- packages/init/lib/installTemplate.js
+++ packages/init/lib/installTemplate.js
@@ -2,6 +2,21 @@ import path from "node:path";
 import fse from "fs-extra";
 import { pathExistsSync } from "path-exists";
 import { log } from "@asfor-cli/utils";
+import ora from "ora";
+
+function getCacheFilePath(targetPath, template) {
+  return path.resolve(targetPath, "node_modules", template.npmName, 'template');
+}
+function copyFile(targetPath, template, installDir) {
+  const originFile = getCacheFilePath(targetPath, template);
+  const fileList = fse.readdirSync(originFile)
+  const spinner = ora('copy template').start()
+  fileList.map(file => {
+    fse.copySync(`${originFile}/${file}`, `${installDir}/${file}`)
+  })
+  spinner.stop()
+  spinner.succeed('copy template success')
+}
 
 export default function installTemplate(selectedTemplate, opts) {
   const { force = false } = opts;
@@ -12,19 +27,20 @@ export default function installTemplate(selectedTemplate, opts) {
 
   fse.ensureDirSync(targetPath);
 
-  const installDirSync = path.resolve(`${rootDir}/${name}`);
-  log.verbose("installDirSync", installDirSync);
-  
-  if (pathExistsSync(installDirSync)) {
+  const installDir = path.resolve(`${rootDir}/${name}`);
+  log.verbose("installDir", installDir);
+
+  if (pathExistsSync(installDir)) {
     if (!force) {
       log.error(`${name} already exists, please use --force to overwrite.`);
       return;
     } else {
       // 先移除再安装
-      fse.removeSync(installDirSync);
-      fse.ensureDirSync(installDirSync);
+      fse.removeSync(installDir);
+      fse.ensureDirSync(installDir);
     }
   } else {
-    fse.ensureDirSync(installDirSync);
+    fse.ensureDirSync(installDir);
   }
+  copyFile(targetPath, template, installDir);
 }
