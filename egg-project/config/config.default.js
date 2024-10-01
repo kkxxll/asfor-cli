4e0f4a39dfd79b8b85aba59684b023649aa4f534
kkxxll <fflinkexin@chinatelecom.cn>
2024-10-01 10:03:46 +0800

    feat:mongo

--- egg-project/config/config.default.js
+++ egg-project/config/config.default.js
@@ -8,7 +8,14 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = exports = {
+    mongoose: {
+      url: 'mongodb://127.0.0.1:27017/asfor-cli',
+      // url: 'mongodb://kk:123456@127.0.0.1:27017/asfor',
+    }
+  };
+
+  
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1727580362425_7996';
