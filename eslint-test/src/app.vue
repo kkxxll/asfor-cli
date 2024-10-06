2649c06c0b534aaaf948b76cf5ec7957805b7bc5
kkxxll <fflinkexin@chinatelecom.cn>
2024-10-06 12:27:08 +0800

    feat:eslint

---
+++ eslint-test/src/app.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        123
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- packages/cli/lib/index.js
+++ packages/cli/lib/index.js
@@ -1,11 +1,13 @@
 import createCLI from "./createCLI.js";
 import createInitCommand from "@asfor-cli/init";
 import installCommand from "@asfor-cli/install";
+import createLintCommand from "@asfor-cli/lint";
 import "./exception.js";
 
 export default function (args) {
   const program = createCLI();
   createInitCommand(program);
   installCommand(program);
+  createLintCommand(program);
   program.parse(process.argv)
 }
